c0a317b8bee5b60d637eb7bd5c87612f840e79a3
Martin Bruse <zondolfin@gmail.com>
2013-06-13 14:40:47 +0200

    working collections it seems

--- kc/kc.go
+++ kc/kc.go
@@ -1,53 +1,79 @@
 package kc
 
 import (
+	"bitbucket.org/ww/cabinet"
+	"bytes"
 	"fmt"
-	"github.com/fsouza/gokabinet/kc"
 )
 
-func keyCombine(key1, key2 string) string {
-	return fmt.Sprintf("%v\000%v", key1, key2)
+func keyCombine(key1, key2 []byte) (result []byte) {
+	result = make([]byte, len(key1)+len(key2)+1)
+	copy(result, key1)
+	copy(result[len(key1)+1:], key2)
+	return
+}
+
+type KV struct {
+	Key   []byte
+	Value []byte
 }
 
 type DB struct {
-	kc.DB
+	cabinet.KCDB
 }
 
 func New(path string) (result *DB, err error) {
-	var kdb *kc.DB
-	if kdb, err = kc.Open(fmt.Sprintf("%v.kct", path), kc.WRITE); err != nil {
+	kcdb := cabinet.New()
+	if err = kcdb.Open(fmt.Sprintf("%v.kct", path), cabinet.KCOWRITER|cabinet.KCOCREATE); err != nil {
 		return
 	}
 	result = &DB{
-		DB: *kdb,
+		KCDB: *kcdb,
 	}
 	return
 }
 
-func (self *DB) SubSetInt(key1, key2 string, value int) error {
-	return self.DB.SetInt(keyCombine(key1, key2), value)
+func (self *DB) SubSet(key1, key2, value []byte) error {
+	return self.KCDB.Set(keyCombine(key1, key2), value)
 }
 
-func (self *DB) SubGetInt(key1, key2 string) (int, error) {
-	return self.DB.GetInt(keyCombine(key1, key2))
+func (self *DB) SubGet(key1, key2 []byte) ([]byte, error) {
+	return self.KCDB.Get(keyCombine(key1, key2))
 }
 
-func (self *DB) SubSetGob(key1, key2 string, value interface{}) error {
-	return self.DB.SetGob(keyCombine(key1, key2), value)
+func (self *DB) SubRemove(key1, key2 []byte) error {
+	return self.KCDB.Remove(keyCombine(key1, key2))
 }
 
-func (self *DB) SubGetGob(key1, key2 string, result interface{}) error {
-	return self.DB.GetGob(keyCombine(key1, key2), result)
+func (self *DB) SubIncrDouble(key1, key2 []byte, delta float64) error {
+	return self.KCDB.IncrDouble(keyCombine(key1, key2), delta)
 }
 
-func (self *DB) SubSet(key1, key2, value string) error {
-	return self.DB.Set(keyCombine(key1, key2), value)
+func (self *DB) SubIncrInt(key1, key2 []byte, delta int64) (int64, error) {
+	return self.KCDB.IncrInt(keyCombine(key1, key2), delta)
 }
 
-func (self *DB) SubGet(key1, key2 string) (string, error) {
-	return self.DB.Get(keyCombine(key1, key2))
-}
-
-func (self *DB) SubRemove(key1, key2 string) error {
-	return self.DB.Remove(keyCombine(key1, key2))
+func (self *DB) GetCollection(key1 []byte) (result []KV) {
+	cursor := self.KCDB.Cursor()
+	var err error
+	if err = cursor.JumpKey(key1); err != nil {
+		panic(err)
+	}
+	for {
+		key, value, err := cursor.Get(true)
+		if err != nil {
+			panic(err)
+		}
+		if len(key) <= len(key1) || key[len(key1)] != 0 || bytes.Compare(key[:len(key1)], key1) != 0 {
+			break
+		}
+		result = append(result, KV{
+			Key:   key[len(key1)+1:],
+			Value: value,
+		})
+	}
+	if err != nil {
+		panic(err)
+	}
+	return
 }
--- kc/kc_test.go
+++ kc/kc_test.go
@@ -10,43 +10,42 @@ func TestCrud(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 	defer d.Close()
-	if err := d.SubSet("k1", "k2", "v"); err != nil {
+	if err := d.SubSet([]byte("k1"), []byte("k2"), []byte("v")); err != nil {
 		t.Errorf(err.Error())
 	}
-	if v, err := d.SubGet("k1", "k2"); v != "v" || err != nil {
+	if v, err := d.SubGet([]byte("k1"), []byte("k2")); string(v) != "v" || err != nil {
 		t.Errorf("Wrong value!")
 	}
-	if err := d.SubRemove("k1", "k2"); err != nil {
+	if err := d.SubRemove([]byte("k1"), []byte("k2")); err != nil {
 		t.Errorf(err.Error())
 	}
-	if v, err := d.SubGet("k1", "k2"); v == "v" || err == nil {
+	if v, err := d.SubGet([]byte("k1"), []byte("k2")); string(v) == "v" || err == nil {
 		t.Errorf("Not removed!")
 	}
+}
 
-	if err := d.SubSetInt("k1", "k4", 33); err != nil {
-		t.Errorf(err.Error())
+func TestCollection(t *testing.T) {
+	d, err := New("test")
+	if err != nil {
+		t.Fatal(err.Error())
 	}
-	if v2, err := d.SubGetInt("k1", "k4"); v2 != 33 || err != nil {
-		t.Errorf("Wrong value!")
+	defer d.Close()
+	d.SubSet([]byte("a"), []byte("b"), []byte("1"))
+	d.SubSet([]byte("z"), []byte("b"), []byte("1"))
+	d.SubSet([]byte("x"), []byte("b"), []byte("c"))
+	d.SubSet([]byte("x"), []byte("c"), []byte("d"))
+	d.SubSet([]byte("x"), []byte("d"), []byte("e"))
+	coll := d.GetCollection([]byte("x"))
+	if len(coll) != 3 {
+		t.Errorf("Wanted 3 elements")
 	}
-	if err := d.SubRemove("k1", "k4"); err != nil {
-		t.Errorf(err.Error())
+	if string(coll[0].Key) != "b" || string(coll[0].Value) != "c" {
+		t.Errorf("Wrong value")
 	}
-	if v2, err := d.SubGetInt("k1", "k4"); v2 == 33 || err == nil {
-		t.Errorf("Not removed!")
+	if string(coll[1].Key) != "c" || string(coll[1].Value) != "d" {
+		t.Errorf("Wrong value")
 	}
-
-	if err := d.SubSetGob("k1", "k3", "v2"); err != nil {
-		t.Errorf(err.Error())
-	}
-	var v string
-	if err := d.SubGetGob("k1", "k3", &v); v != "v2" || err != nil {
-		t.Errorf("Wrong value!")
-	}
-	if err := d.SubRemove("k1", "k3"); err != nil {
-		t.Errorf(err.Error())
-	}
-	if err := d.SubGetGob("k1", "k3", &v); v == "v" || err == nil {
-		t.Errorf("Not removed!")
+	if string(coll[2].Key) != "d" || string(coll[2].Value) != "e" {
+		t.Errorf("Wrong value")
 	}
 }
