ce3595dbed15e13e58d447c97437a9f2f6a096b7
Martin Bruse <zondolfin@gmail.com>
2013-09-13 08:49:21 +0200

    put more intelligence in the subs package, simplifying external usage of it

--- subs/subscription.go
+++ subs/subscription.go
@@ -4,7 +4,12 @@ import (
 	"code.google.com/p/go.net/websocket"
 	"fmt"
 	"github.com/zond/kcwraps/kol"
+	"reflect"
 	"sync"
+)
+
+const (
+	FetchType = "Fetch"
 )
 
 type Object struct {
@@ -18,16 +23,14 @@ type Message struct {
 }
 
 type Subscription struct {
-	pack *Pack
-	url  string
-	name string
+	pack  *Pack
+	url   string
+	name  string
+	Query *kol.Query
+	Call  func(o interface{}, op string)
 }
 
-func (self *Subscription) Name() string {
-	return self.name
-}
-
-func (self *Subscription) Call(i interface{}, op string) {
+func (self *Subscription) Send(i interface{}, op string) {
 	if err := websocket.JSON.Send(self.pack.ws, Message{
 		Type: op,
 		Object: Object{
@@ -36,6 +39,41 @@ func (self *Subscription) Call(i interface{}, op string) {
 		},
 	}); err != nil {
 		self.pack.unsubscribeName(self.name)
+	}
+}
+
+func (self *Subscription) DB() *kol.DB {
+	return self.pack.db
+}
+
+func (self *Subscription) Subscribe(object interface{}) {
+	if self.Query == nil {
+		if err := self.pack.db.Subscribe(self.name, object, kol.AllOps, func(i interface{}, op kol.Operation) {
+			self.Call(i, op.String())
+		}); err != nil {
+			panic(err)
+		}
+		if err := self.pack.db.Get(object); err != nil {
+			if err != kol.NotFound {
+				panic(err)
+			}
+		} else {
+			self.Call(object, FetchType)
+		}
+	} else {
+		if err := self.Query.Subscribe(self.name, object, kol.AllOps, func(i interface{}, op kol.Operation) {
+			slice := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(object)), 1, 1)
+			slice.Index(0).Set(reflect.ValueOf(i))
+			self.Call(slice.Interface(), op.String())
+		}); err != nil {
+			panic(err)
+		}
+		slice := reflect.New(reflect.SliceOf(reflect.TypeOf(object))).Interface()
+		if err := self.Query.All(slice); err != nil {
+			panic(err)
+		} else {
+			self.Call(reflect.ValueOf(slice).Elem().Interface(), FetchType)
+		}
 	}
 }
 
@@ -81,10 +119,12 @@ func (self *Pack) UnsubscribeAll() {
 	self.subs = make(map[string]*Subscription)
 }
 
-func (self *Pack) New(url string) *Subscription {
-	return &Subscription{
+func (self *Pack) New(url string) (result *Subscription) {
+	result = &Subscription{
 		pack: self,
 		url:  url,
 		name: self.generateName(url),
 	}
+	result.Call = result.Send
+	return
 }
